b3690cf6f6ecd43019e181db4cc0a8985ba81413
Hridoy sultan <Hridoysultan.official@gmail.comm>
2021-07-20 13:58:47 +0300

    shoplex for assignment-10

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const nodemon = require('nodemon');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const port = 5055;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ucd7g.mongodb.net/${process.env.DB_Name}?retryWrites=true&w=majority`;
+console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    console.log('connection err', err);
+
+    const ProductCollection = client.db("shopplex").collection("shopplexCollection");
+    console.log('database connected');
+
+    app.get('/product', (req, res) => {
+        ProductCollection.find()
+            .toArray((err, items) => {
+                // console.log('item', items);
+                res.send(items)
+
+            })
+    });
+
+    app.post('/addProduct', (req, res) => {
+        const product = req.body;
+        ProductCollection.insertOne(product)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+
+            })
+
+    })
+
+});
+
+app.get('/', (req, res) => {
+    res.send('hello world h!');
+    console.log('hello w');
+
+});
+
+
+
+
+app.listen(port);
+
